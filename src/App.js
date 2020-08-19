ff16003220973c14d88882e750d7e9050e138679
Megan Horsfield <meganthadev@gmail.com>
2020-08-19 05:45:48 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import BlogPost from './BlogPost.js'
 import ColorBox from './ColorBox.js'
@@ -23,3 +24,30 @@ class App extends Component {
 }
 
 export default App;
+=======
+import React, { Component } from 'react';
+import BlogPost from './BlogPost.js'
+import ColorBox from './ColorBox.js'
+import Comment from './Comment.js'
+
+class App extends Component {
+  
+  // Nothing needs to change here! 
+  // Make note of what prop we are initially passing to `ColorBox` below!
+  
+  render() {
+    return (
+      <div id="app">
+          <BlogPost />
+            <Comment />
+              <div id="seperator"></div>
+          <div className="wrapper">
+            <ColorBox opacity={1} />
+          </div>
+      </div>
+    )
+  }
+}
+
+export default App;
+>>>>>>> 9d3c2971b63b57b8e21eb0578e7afd8070389bb0
